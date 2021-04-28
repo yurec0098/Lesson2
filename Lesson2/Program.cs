55febfe823335b823533f707a2f420c9eef69479
yurec0098 <yurec0098@rambler.ru>
2021-04-28 06:58:17 +0300

    Добавьте файлы проекта.

---
+++ Lesson2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lesson2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Добро пожаловать!");
+		}
+
+		static int ReadInt(string text)
+		{
+			int value;
+			Console.WriteLine(text);
+			while (!int.TryParse(Console.ReadLine(), out value))
+				Console.WriteLine($"Повторми... {text}");
+
+			return value;
+		}
+		static float ReadFloat(string text)
+		{
+			float value;
+			Console.WriteLine(text);
+			while (!float.TryParse(Console.ReadLine(), out value))
+				Console.WriteLine($"Повторми... {text}");
+
+			return value;
+		}
+	}
+}
