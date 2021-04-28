138886235ef2399de762c432d107eeb60ba50f4b
yurec0098 <yurec0098@rambler.ru>
2021-04-28 07:25:42 +0300

    Исправление возможных ошибок

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -10,7 +10,7 @@ namespace Lesson2
 		{
 			Console.WriteLine("Добро пожаловать!");
 
-			var month = ReadInt("Введите порядковый номер текущего месяца:");
+			var month = ReadMonthNum("Введите порядковый номер текущего месяца (от 1 до 12):");
 
 			DateTimeFormatInfo formatInfo = CultureInfo.CurrentCulture.DateTimeFormat;
 			Console.WriteLine($"Сейчас месяц {formatInfo.GetMonthName(month)}");
@@ -30,6 +30,16 @@ namespace Lesson2
 			float value;
 			Console.WriteLine(text);
 			while (!float.TryParse(Console.ReadLine(), out value))
+				Console.WriteLine($"Повторми... {text}");
+
+			return value;
+		}
+
+		static int ReadMonthNum(string text)
+		{
+			int value;
+			Console.WriteLine(text);
+			while (!(int.TryParse(Console.ReadLine(), out value) && value > 0 && value <= 12))
 				Console.WriteLine($"Повторми... {text}");
 
 			return value;
