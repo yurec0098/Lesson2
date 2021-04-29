e32cbe99ba297593f364425ce84d7b8d84d07424
yurec0098 <yurec0098@rambler.ru>
2021-04-29 06:52:29 +0300

    Если пользователь указал месяц из зимнего периода, а средняя температура > 0, вывести сообщение «Дождливая зима».

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -10,10 +10,48 @@ namespace Lesson2
 		{
 			Console.WriteLine("Добро пожаловать!");
 
+			float min_temperature = ReadFloat("Введите минимальную температуру за сутки:");
+			float max_temperature = ReadFloat("Введите максимальную температуру за сутки:");
+
+			float average_temperature = (min_temperature + max_temperature) / 2;
+			Console.WriteLine($"Средняя температура за сутки состовляла: {average_temperature:0.0} градусов");
+
 			var month = ReadMonthNum("Введите порядковый номер текущего месяца (от 1 до 12):");
 
 			DateTimeFormatInfo formatInfo = CultureInfo.CurrentCulture.DateTimeFormat;
-			Console.WriteLine($"Сейчас месяц {formatInfo.GetMonthName(month)}");
+			switch (month)
+			{
+				case 1:
+				case 2:
+				case 12:
+					if(average_temperature > 0)
+						Console.WriteLine($"Дождливая зима. Месяц {formatInfo.GetMonthName(month)}");
+					else
+						Console.WriteLine($"Зима. Месяц {formatInfo.GetMonthName(month)}");
+					break;
+
+				case 3:
+				case 4:
+				case 5:
+					Console.WriteLine($"Весна. Месяц {formatInfo.GetMonthName(month)}");
+					break;
+
+				case 6:
+				case 7:
+				case 8:
+					Console.WriteLine($"Лето. Месяц {formatInfo.GetMonthName(month)}");
+					break;
+
+				case 9:
+				case 10:
+				case 11:
+					Console.WriteLine($"Осень. Месяц {formatInfo.GetMonthName(month)}");
+					break;
+
+				default:
+					Console.WriteLine($"Месяц {formatInfo.GetMonthName(month)}");
+					break;
+			}
 		}
 
 		static int ReadInt(string text)
